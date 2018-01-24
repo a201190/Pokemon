d8178e463f51702b23c770091dc46fb7792c94a1
Amit <1593.amit@gmail.com>
2018-01-25 00:28:46 +0530

    change

--- config/keys.js
+++ config/keys.js
@@ -1,8 +1,1 @@
-module.exports = {
-  facebookClientID: '124869444928001',
-  facebookClientSecret: '8e0c52091580ae882dd352129503e1bf',
-  mongoUserName: 'Pokemon',
-  mongoUserPassword: 'Pokemon',
-  mongooseURI: 'ds036577.mlab.com:36577/amitpokemon',
-  cookieKey: 'dkjdnwkdnwkjdwwdjkwnd',
-};
+
